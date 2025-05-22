eff1c1fd44ab839410bc3c2b33462bff94af8216
youngji-song <songyj7825@gmail.com>
2025-05-22 11:44:23 +0900

    login

--- Keepit_Backend/users/serializers.py
+++ Keepit_Backend/users/serializers.py
@@ -5,38 +5,38 @@ from .models import User
 from regions.models import RegionCity, RegionDistrict
 
 class UserCreateSerializer(serializers.ModelSerializer):
-    region_city_name = serializers.CharField(write_only=True)
-    region_district_name = serializers.CharField(write_only=True)
+    region_city = serializers.CharField(write_only=True)
+    region_district = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
         fields = (
             'userid', 'password', 'email', 'nickname', 'name',
             'birth_year', 'birth_month', 'birth_day',
-            'region_city_name', 'region_district_name'
+            'region_city', 'region_district'
         )
         extra_kwargs = {
             'password': {'write_only': True}
         }
 
     def create(self, validated_data):
-        city_name_raw = validated_data.pop('region_city_name')
+        city_name_raw = validated_data.pop('region_city')
         city_name = (
             city_name_raw.replace("특별시", "")
                          .replace("광역시", "")
                          .replace("시", "")
         )
-        district_name = validated_data.pop('region_district_name')
+        district_name = validated_data.pop('region_district')
 
         try:
             city = RegionCity.objects.get(name__icontains=city_name)
         except RegionCity.DoesNotExist:
-            raise serializers.ValidationError({'region_city_name': f'{city_name} 시는 존재하지 않습니다.'})
+            raise serializers.ValidationError({'region_city': f'{city_name} 시는 존재하지 않습니다.'})
 
         try:
             district = RegionDistrict.objects.get(name=district_name, city=city)
         except RegionDistrict.DoesNotExist:
-            raise serializers.ValidationError({'region_district_name': f'{district_name} 는 {city.name}에 존재하지 않습니다.'})
+            raise serializers.ValidationError({'region_district': f'{district_name} 는 {city.name}에 존재하지 않습니다.'})
 
         password = validated_data.pop('password')
         user = User(**validated_data)
--- Keepit_Backend/users/urls.py
+++ Keepit_Backend/users/urls.py
@@ -1,8 +1,15 @@
 # users/urls.py
 
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, MyPageView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('mypage/', MyPageView.as_view()),
 ]
